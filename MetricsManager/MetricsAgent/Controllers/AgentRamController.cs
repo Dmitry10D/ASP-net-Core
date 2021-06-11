4460eb3c2934b68ba86be11658fca2e95255609c
Dmitry <dima10des@gmail.com>
2021-06-11 19:57:22 +0300

    try1

---
+++ MetricsManager/MetricsAgent/Controllers/AgentRamController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/metrics")]
+    [ApiController]
+    public class AgentRamController : ControllerBase
+    {
+        [HttpGet("ram/available")]
+        public IActionResult CollectMetrics()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ MetricsManager/MetricsAgentUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MetricsAgentUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
